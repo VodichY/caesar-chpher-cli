1222b3fd20a791c9446aa3724b969055ddd9f7a6
YVodzich <yuri.vodich@gmail.com>
2021-05-09 00:17:35 +0300

    adding option check

---
+++ app.js
@@ -0,0 +1,40 @@
+const yargs = require('yargs')
+
+programOptions = yargs
+  .option('--shift', {
+    alias: '-s',
+    demandOption: true,
+    describe: 'a shift',
+    type: 'number'
+  })
+  .option('--action', {
+    alias: '-a',
+    demandOption: true,
+    describe: 'an action encode/decode',
+    type: 'string',
+    choices: ['encode','decode']
+  })
+  .option('--input', {
+    alias: '-i',
+    demandOption: false,
+    describe: 'an input file',
+    type: 'string'
+  })
+  .option('--output', {
+    alias: '-o',
+    demandOption: false,
+    describe: 'an output file',
+    type: 'string'
+  }) 
+  .option('--version', {
+    alias: '-v',
+    hidden: true
+  })
+  .option('--help', {
+    alias: '-h',
+    hidden: true
+  })
+  .argv;
+
+  
+
